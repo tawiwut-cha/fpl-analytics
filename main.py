757b423d0bf5f65215d29fd1f32017497169eefa
Arm_Yoga <tawiwut.cha@gmail.com>
2024-08-18 01:02:43 +0300

    2024-25 gw1 can run and clean up code

--- main.py
+++ main.py
@@ -9,22 +9,34 @@ base_url = 'https://fantasy.premierleague.com/api/'
 
 # 1. Get data
 
-## League id 
+## Get gameweek status
+def get_gw_status(gw_no:int):
+    r = requests.get(base_url+'bootstrap-static/')
+    assert r.status_code == 200, r.status_code
+    return None
 
+## League id
 def get_league_id(league_name):
     league_ids = {
-        'rbsc': 532134,
-        'rpk': 766550,
-        'ifc': 805760,
+        'rbsc': 584167,
+        'rpk': 748775,
+        'ifc': 898742,
     }
     try:
         return league_ids[league_name]
     except KeyError:
         print('Please enter a valid league name', league_ids.keys)
 
+## Player data
+def get_dim_players():
+    # get latest player info
+    r = requests.get(base_url+'bootstrap-static/')
+    assert r.status_code == 200, r.status_code
+    return pd.DataFrame(r.json()['elements'])
+
 ## Manager data
 
-def get_league_manager_id(league_id:int):
+def get_league_manager_id(league_id:int) -> list:
     '''get manager ids from a classic league'''
     manager_id = []
     page_no = 1
@@ -39,11 +51,25 @@ def get_league_manager_id(league_id:int):
             manager_id.append(entry['entry'])
     return manager_id
 
+def get_dim_managers(league_id:int):
+    '''get manager info from that classic league'''
+    manager_ids = get_league_manager_id(league_id)
+    data = []
+    for manager_id in manager_ids:
+        manager_info = get_manager_info(manager_id)
+        # delete unwanted info
+        del(manager_info['leagues'])
+        del(manager_info['kit'])
+        data.append(manager_info)
+    return pd.DataFrame(data)
+
 def get_manager_info(manager_id:int) -> dict:
     '''Basic info on FPL Manager'''
     r = requests.get(base_url+'entry/'+str(manager_id))
     assert r.status_code == 200, r.status_code
     return r.json() 
+
+## Manager Picks
 
 def get_manager_picks(manager_id:int, gw_no:int) -> dict:
     '''Squad picks of manager for a GW.'''
@@ -57,10 +83,25 @@ def get_manager_transfers(manager_id:int, gw_no:int) -> dict:
     assert r.status_code == 200, r.status_code
     return r.json() 
 
-## Player data
+## GW Picks data
 
-def get_players():
-    return pd.read_csv('data/player_idlist.csv')
+def get_gw_picks(league_id, gw_no):
+    '''Returns a dataframe of selected players.'''
+    # manager_ids = get_league_manager_id(league_id)
+    # gw_picks = pd.DataFrame()
+    # for manager_id in manager_ids:
+    #     gw_pick = pd.DataFrame(get_manager_picks(manager_id, gw_no)['picks'])
+    #     gw_pick['manager_id'] = manager_id # add manager_id column as label
+    #     gw_picks = pd.concat([gw_picks, gw_pick], ignore_index=True)
+    # return gw_picks
+    manager_ids = get_league_manager_id(league_id)
+    gw_picks = []
+    for manager_id in manager_ids:
+        gw_pick = get_manager_picks(manager_id, gw_no)['picks']
+        for pick in gw_pick:
+            pick['manager_id'] = manager_id
+        gw_picks += gw_pick
+    return pd.DataFrame(gw_picks)
 
 ## GW Points data
 
@@ -86,28 +127,6 @@ def get_gw_points(league_id, gw_no):
     df = pd.DataFrame(data)
     df['rank'] = df['h2h_points'].rank(method='dense', ascending=False).astype(int)
     return df.sort_values(by='rank', ascending=True)
-
-## GW Picks data
-
-def get_gw_picks(league_id, gw_no):
-    '''Returns a dataframe of selected players.'''
-    manager_ids = get_league_manager_id(league_id)
-    data = pd.DataFrame()
-    for manager_id in manager_ids:
-        manager_data = pd.DataFrame(get_manager_picks(manager_id, gw_no)['picks'])
-        manager_data['manager_id'] = manager_id
-        data = pd.concat([data, manager_data], ignore_index=True)
-    return data
-
-
-def get_gw_captains(league_id, gw_no):
-    '''Returns a dataframe of captains and number of managers captaining each.'''
-    pass
-
-
-def get_gw_transfers(league_id, gw_no):
-    '''Returns a dataframe of transfers and the count of how many.'''
-    pass
 
 # 2. PNL calc
 
@@ -174,76 +193,15 @@ def pnl_ifc(gw:pd.DataFrame):
     return gw
 
 ## Respect Kruwai
-def unique_rank_pnl(ranks:list):
-    pnl = []
-    for i in ranks:
-        if i == 1:
-            pnl.append(300)
-        elif i == 2:
-            pnl.append(200)
-        elif i == 3:
-            pnl.append(100)
-        elif i == 4:
-            pnl.append(-100)
-        elif i == 5:
-            pnl.append(-200)
-        elif i == 6:
-            pnl.append(-300)
-    return pnl
-
-def get_winners_ratios_equal_rank(winners_ranks):
-    winners_ranks = list(sorted(winners_ranks))
-    if winners_ranks == [1,2,3]:
-        return {1:3/6, 2:2/6, 3:1/6}
-    elif winners_ranks == [1,2,2]:
-        return {1:6/12, 2:3/12}
-    elif winners_ranks == [1,1,2]:
-        return {1:5/12, 2:2/12}
-    elif winners_ranks == [1,1,1]:
-        return {1:4/12}
-    elif winners_ranks == [1,2]:
-        return {1:8/12, 2:4/12}
-    elif winners_ranks == [1,1]:
-        return {1:1/2}
-    elif winners_ranks == [1]:
-        return {1:1}
-
-def equal_rank_pnl(ranks:list):
-    max_rank = max(ranks)
-    assert max_rank > 3, 'Max rank < 3 not applicable for this logic'
-    pnl = [0]*len(ranks)
-
-    # losers
-    for i in range(len(ranks)):
-        if ranks[i] == max_rank:
-            pnl[i] = -300
-        elif ranks[i] == max_rank - 1:
-            pnl[i] = -200
-        elif ranks[i] == max_rank - 2:
-            pnl[i] = -100
-
-    # winners 
-    total_prize = sum(pnl) * -1
-    # get sorted list of winners' rank
-    winners_ranks = [ranks[i] for i in range(len(ranks)) if ranks[i] < max_rank - 2]
-    winners_ratio = get_winners_ratios_equal_rank(winners_ranks)
-    for i in range(len(pnl)):
-        if pnl[i] == 0:
-            pnl[i] = round(winners_ratio[ranks[i]] * total_prize)
-    assert sum(pnl) == 0
-    return pnl
 
 def pnl_rpk(gw):
     gw = gw.copy()
     if max(gw['rank']) == len(gw):
+        gw['pnl'] = [300,200,100,0,0,-100,-200,-300] # hardcode
         gw['is_unique_rank'] = True
-        gw['pnl'] = unique_rank_pnl(list(gw['rank']))
     else:
+        gw['pnl'] = np.nan
         gw['is_unique_rank'] = False
-        try:
-            gw['pnl'] = equal_rank_pnl(list(gw['rank']))   
-        except:
-            gw['pnl'] = np.nan
     return gw
 
 # 3. Data writing
